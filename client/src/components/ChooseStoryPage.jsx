0ca866686d867ba9416c7311b3cc8c73beb69a65
Eilon Alter <eilon159123@gmail.com>
2024-06-14 23:30:59 +0300

    add server req from client

--- client/src/components/ChooseStoryPage.jsx
+++ client/src/components/ChooseStoryPage.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import backgroundvideo from './../assets/background.mp4'
+import axios from 'axios';
+
+
 
 const ChooseStoryPage = () => {
+
+    const [story, setStory] = useState('random song');
 
     const [isChecked, setIsChecked] = useState(false);
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
-      };
+    };
+
+    const handleStoryChange = (e) => {
+        if (e.target.value === '') {
+            setStory('random song');
+        }
+        setStory(e.target.value);
+        console.log(story);
+
+    };
+
+    const handleNextButton = () => {
+        axios.post('http://localhost:5000/api/chat', { message: story })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
 
     return (
         <div className='storyContainer'>
@@ -18,21 +42,23 @@ const ChooseStoryPage = () => {
             </div>
             <div>
                 <div className='chooseSong'>
-                    <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange}/>
+                    <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
                     <p className='checkboxIsStory'>A song based on your story</p>
                 </div>
                 {isChecked && (
-                    <textarea placeholder='Write your story' className='storyText'/>
+                    <textarea placeholder='Write your story' className='storyText' onChange={(e) => {
+                        handleStoryChange(e);
+                    }} />
                 )}
                 <div className='chooseSong'>
-                    <input type="checkbox" checked={!isChecked} onChange={handleCheckboxChange}/>
+                    <input type="checkbox" checked={!isChecked} onChange={handleCheckboxChange} />
                     <p className='checkboxIsStory'>Random song</p>
                 </div>
             </div>
-            
+
             <video autoPlay loop muted playsInline src={backgroundvideo} className='backgroundvideo'></video>
-            
-            <Link to="/choosegenre"><button>Next</button></Link>
+
+            <Link to="/choosegenre"><button onClick={handleNextButton} >Next</button></Link>
         </div>
     );
 }
